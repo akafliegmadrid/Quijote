de70d0a2447b900b97405d78baa84c9d2059d513
Andrés Mateo <andresmateo.mg@gmail.com>
2020-02-21 14:21:45 +0100

    Añadidos entradas y salidas de la funcion ala.m

--- ala.m
+++ ala.m
@@ -1,5 +1,16 @@
-function [ out ] = ala( in )
+function [ CL, CD ] = ala( n, d, t, c, phi, b, perfil, Vinf, h, AoA, Beta )
 % ALA calcula las propiedades del ala a partir de la geometria (y el perfil?)
+%   n      -> numero de secciones
+%   d      -> angulos de diedro de las secciones
+%   t      -> angulos de torsion en el encastre, quiebros y punta del ala
+%   c      -> cuerda en el encastre, quiebros y punta del ala
+%   phi    -> flecha del borde de ataque de las secciones
+%   b      -> longitud de las secciones
+%   perfil -> coordenadas de los perfiles en cada cambio de seccion (n+1)x2
+%   Vinf   -> velocidad de vuelo
+%   h      -> altitud de vuelo ISA
+%   AoA    -> angulo de ataque
+%   Beta   -> angulo de guiñada
 %
 %   Participantes:
 %       - Andres Mateo Gabin
