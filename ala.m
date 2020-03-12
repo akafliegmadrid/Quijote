b6dcc41f654c9b6454b3aad0ca4f42808c57d434
Andryw4 <andresmateo.mg@gmail.com>
2020-03-12 21:07:05 +0100

    Terminado modulo del ala, merge para continuar con las pruebas

--- ala.m
+++ ala.m
@@ -1,4 +1,4 @@
-function[ CL, CD ] = ala (d_q, t_q, c_q, phi_q, b_q)
+function[ CL, CD ] = ala (d_q, t_q, c_q, phi_q, b_q, vinf, h, alpha)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ALA calcula las propiedades del ala a partir de la geometria
@@ -7,50 +7,53 @@ function[ CL, CD ] = ala (d_q, t_q, c_q, phi_q, b_q)
 %       - Andres Mateo Gabin
 %       - Alesander Oteo Torres
 
-% Variables para definir la geometria :
-% d_q   -> diedro entre quiebros desde el encastre, np componentes
-% t_q   -> torsion en cada quiebro desde el q0,np+1 componentes
-% c_q   -> cuerda en cada quiebro, np+1 componentes
-% phi_q -> flecha 1/4 de los quiebros desde el encastre, np componentes
-% b_q   -> longitud de cada quiebro, np componentes
+% Variables para definir la geometria:
+% d_q   -> diedro entre quiebros desde el encastre, NSECCIONES componentes
+% t_q   -> torsion en cada quiebro desde el q0, NSECCIONES+1 componentes
+% c_q   -> cuerda en cada quiebro, NSECCIONES+1 componentes
+% phi_q -> flecha 1/4 de los quiebros desde el encastre, NSECCIONES comps.
+% b_q   -> longitud de cada quiebro, NSECCIONES componentes
 
-% NOTA: para evitar confusiones, el 1 siempre hará referencia al encastre o
-% al primer quiebro. si el ala tiene 3 quiebros, el c_q(1) será la cuerda
-% en el encastre, y el b_q(1) será lo que mide el primer quiebro, es decir,
+% Variables para definir las condiciones de vuelo:
+% vinf  -> velocidad de vuelo en m/s
+% h     -> altitud de vuelo en m
+% alpha -> rango de AoA a analizar [min delta max] en grados
+
+% NOTA: para evitar confusiones, el 1 siempre hara referencia al encastre o
+% al primer quiebro. si el ala tiene 3 quiebros, el c_q(1) sera la cuerda
+% en el encastre, y el b_q(1) sera lo que mide el primer quiebro, es decir,
 % desde c_q(1) hasta c_q(2)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Numero de paneles
-np = 3;           % no. de quiebros
-nx = 10;          % direccion de la cuerda
-ny = [40 20 10];  % direccion de la envergadura
+% Parametros globales
+global NSECCIONES NPANELX NPANELY
 
 % Punto de referencia
-xRef = 0.25 * c_q(1)
-yRef = 0.0
-zRef = 0.0
+xRef = 0.25 * c_q(1);
+yRef = 0.0;
+zRef = 0.0;
 
 % Centro de masas
 % TODO: calcularlo en funcion de la forma de las alas
-xCG = xRef
-yCG = yRef
-zCG = zRef
+xCG = xRef;
+yCG = yRef;
+zCG = zRef;
 
 % Estrechamientos
-taperRatio = c_q(2:end) / c_q(1:end-1)
+taperRatio = c_q(2:end) / c_q(1:end-1);
 
 % Structs iniciales para las funciones de Tornado
-geo.meshtype  = ones(1,np);
+geo.meshtype  = ones(1,NSECCIONES);
 latticetype   = 1;                 % Standard horseshoe lattice
 geo.symetric  = 1;                 % 1: Ala entera,
 geo.nwing     = 1;                 % Numero de alas
-geo.nelem     = np;                % Numero de particiones del ala
+geo.nelem     = NSECCIONES;        % Numero de particiones del ala
 geo.ref_point = [xRef yRef zRef];  % Punto de referencia
 geo.CG        = [xCG yCG zCG];     % Centro de gravedad del avion 
 geo.c         = c_q(1);            % Cuerda encastre
 geo.b         = b_q;               % Semienvergaduras del ala
-geo.startx    = 0.0;               % Poiscion segun X del BA del perfil central
+geo.startx    = 0.0;               % Posicion segun X del BA del encastre
 geo.starty    = 0.0;               % Posicion segun Y "..."
 geo.startz    = 0.0;               % Posicion segun Z "..."
 geo.dihed     = d_q;               % Diedro total ala
@@ -58,48 +61,52 @@ geo.T = taperRatio;                % Estrechamiento ala
 geo.SW = phi_q;                    % Flecha ala
 geo.TW(:,:,1) = t_q(1:end-1);      % TODO: por que hay dos dimensiones? 
 geo.TW(:,:,2) = t_q(2:end);
-geo.nx = nx*ones(1,np);            % No. de paneles en la direccion X
-geo.ny = ny;                       % No. de paneles en la direccion Y
+geo.nx = NPANELX*ones(1,NSECCIONES);   % No. de paneles en la direccion X
+geo.ny = NPANELY;                      % No. de paneles en la direccion Y
 
-geo.flapped = zeros(1,np);         % No se utilizan flaps
-geo.fsym = zeros(1,np);            % '...'
-geo.fnx = zeros(1,np);             % '...'
-geo.fc = zeros(1,np);              % '...'
-geo.flap_vector= zeros(1,np);      % '...'
+geo.flapped = zeros(1,NSECCIONES);     % No se utilizan flaps
+geo.fsym = zeros(1,NSECCIONES);        % '...'
+geo.fnx = zeros(1,NSECCIONES);         % '...'
+geo.fc = zeros(1,NSECCIONES);          % '...'
+geo.flap_vector= zeros(1,NSECCIONES);  % '...'
 
 % poner el nombre del perfil que genere el modulo perfil
-geo.foil(:,:,1)= repmat({'akm-01.dat'}, ,np);
-geo.foil(:,:,2)= repmat({'akm-o1.dat'}, ,np);
+geo.foil(:,:,1)= repmat({'akm-01.dat'}, 1, NSECCIONES);
+geo.foil(:,:,2)= repmat({'akm-01.dat'}, 1, NSECCIONES);
 
-% ajustar estos datos para ñas condiciones de cada analisis
-state.AS =     40;        %Airspeed m/s
-state.alpha =  0.05;      %Angle of attack, radians
-state.betha =  0;         %Angle of sideslip, radians
-state.P =      0;         %Rollrate, rad/s
-state.Q =      0;         %pitchrate, rad/s
-state.R =      0;         %yawrate, rad/s
-state.adot =   0;         %Alpha time derivative rad/s
-state.bdot =   0;         %Betha time derivative rad/s
-state.ALT =    0;         %Altitude, m
-state.rho =    1.225;     %Density, kg/m^3
-state.pgcorr = 1;         %Apply prandtl glauert compressibility
-                          %correction
-% Calculo de la eficiencia maxima y el volumen
-%Condiciones atmosfericas
-[~, a, ~, ~] = ISAtmosphere(state.ALT);
-Mach = state.AS/a;
+% Condiciones atmosfericas
+[rho, a, ~, ~] = ISAtmosphere(h);
+Mach = vinf/a;
 
-%Mallado
-[lattice, ref] = fLattice_setup2(geo, state, latticetype);
+% Condiciones de vuelo
+alpha    = deg2rad(alpha);
+alphaAvg = (alpha(1)+alpha(3)) / 2.0;
 
-%Resistencia parasita, superficie mojada y volumen
+state.AS     = vinf;      % Airspeed m/s
+state.alpha  = alphaAvg;  % Angle of attack, radians
+state.betha  = 0;         % Angle of sideslip, radians
+state.P      = 0;         % Rollrate, rad/s
+state.Q      = 0;         % pitchrate, rad/s
+state.R      = 0;         % yawrate, rad/s
+state.adot   = 0;         % Alpha time derivative rad/s
+state.bdot   = 0;         % Betha time derivative rad/s
+state.ALT    = 0;         % Altitude, m
+state.rho    = rho;       % Density, kg/m^3
+state.pgcorr = 0;         % Apply prandtl glauert compressibility
+                          % correction
+
+% Calculo de la eficiencia maxima
+
+% Mallado
+[~, ref] = fLattice_setup2(geo, state, latticetype);
+
+% Resistencia parasita, superficie mojada y volumen
 [CD0_wing, results.Re, results.Swet, results.Vol] = zeroliftdragpred(Mach, state.ALT, geo, ref);
 
-%Calculo de CL y CD (inducida)
-Alpha = [alpha0, d_alpha, alphaf];
-[CL, CD] = solverloop5(results, state, geo, Alpha);
+% Calculo de CL y CD (inducida)
+[CL, CD] = solverloop5(results, state, geo, alpha);
 
-%Calculo de la resistencia total
+% Calculo de la resistencia total
 CD = CD + sum(CD0_wing);
 
-end function
+end
